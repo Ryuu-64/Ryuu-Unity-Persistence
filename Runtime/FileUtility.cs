18382f22f25eba1375e0fc68087f6d6d3500f02e
Ryuu-64 <64ryuu@gmail.com>
2024-02-29 19:56:21 +0800

    Initial commit

---
+++ Runtime/FileUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Ryuu.Unity.Persistence
+{
+    public static class FileUtility
+    {
+        public static string Read(string path)
+        {
+            string fullPath = Combine(Application.persistentDataPath, path);
+            return File.ReadAllText(fullPath);
+        }
+
+        public static async Task<string> ReadAsync(string path, Action afterRead = null)
+        {
+            string fullPath = Combine(Application.persistentDataPath, path);
+            string text = await File.ReadAllTextAsync(fullPath);
+            afterRead?.Invoke();
+            return text;
+        }
+
+        public static void Write(string path, string contents)
+        {
+            string fullPath = Combine(Application.persistentDataPath, path);
+            string directoryPath = Path.GetDirectoryName(fullPath);
+            if (string.IsNullOrEmpty(directoryPath))
+            {
+                throw new ArgumentException($"Invalid file path, path={path}");
+            }
+
+            Directory.CreateDirectory(directoryPath);
+            File.WriteAllText(fullPath, contents);
+        }
+
+        public static async void WriteAsync(string path, string contents, Action afterWrite = null)
+        {
+            string fullPath = Combine(Application.persistentDataPath, path);
+            string directoryPath = Path.GetDirectoryName(fullPath);
+            if (string.IsNullOrEmpty(directoryPath))
+            {
+                throw new ArgumentException($"Invalid file path, path={path}");
+            }
+
+            Directory.CreateDirectory(directoryPath);
+            await File.WriteAllTextAsync(fullPath, contents);
+            afterWrite?.Invoke();
+        }
+
+        private static string Combine(string path1, string path2)
+        {
+            return Path.Combine(ReplaceBackSlashWithForwardSlash(path1), TrimStart(path2));
+        }
+
+        private static string TrimStart(string path)
+        {
+            return ReplaceBackSlashWithForwardSlash(path).TrimStart('/');
+        }
+
+        private static string ReplaceBackSlashWithForwardSlash(string path)
+        {
+            return path.Replace('\\', '/');
+        }
+    }
+}
---
+++ Runtime/PersistenceUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using Ryuu.Unity.Serialization.Json;
+
+namespace Ryuu.Unity.Persistence
+{
+    public static class PersistenceUtility
+    {
+        public static T Read<T>(string path, Func<T> fallbackProvider)
+        {
+            try
+            {
+                string jsonString = FileUtility.Read(path);
+                return JsonUtility.Deserialize(jsonString, fallbackProvider);
+            }
+            catch (Exception)
+            {
+                return fallbackProvider.Invoke();
+            }
+        }
+
+        public static void Write(string path, object value)
+        {
+            string jsonString = JsonConvert.SerializeObject(value);
+            FileUtility.Write(path, jsonString);
+        }
+    }
+}
